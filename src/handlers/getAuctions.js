0539276e9f0f351b7bd8dda9b20c40eedb8db99e
salahmander <salahabdo@hotmail.co.uk>
2021-02-03 17:44:21 +0000

    Create common middleware to reduce code duplication.

--- src/handlers/getAuctions.js
+++ src/handlers/getAuctions.js
@@ -1,8 +1,5 @@
 import AWS from "aws-sdk";
-import middy from "@middy/core";
-import httpJsonBodyParser from "@middy/http-json-body-parser";
-import httpEventNormalizer from "@middy/http-event-normalizer";
-import httpErrorHandler from "@middy/http-error-handler";
+import commonMiddleware from "../lib/commonMiddleware";
 import createError from "http-errors";
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
@@ -32,7 +29,4 @@ async function getAuction(event, context) {
   };
 }
 
-export const handler = middy(getAuction)
-  .use(httpJsonBodyParser()) // Automatically parse stringified event body
-  .use(httpEventNormalizer()) // Automatically adjust the API gateway event objects
-  .use(httpErrorHandler()); // Helpes hanlding error process
+export const handler = commonMiddleware(getAuction);
