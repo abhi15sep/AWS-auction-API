fbcc7b543cb3d7220139843219a28732da51714a
salahmander <salahabdo@hotmail.co.uk>
2021-02-03 17:00:17 +0000

    Create new handler to fetch auction by id.

---
+++ src/handlers/getAuction.js
@@ -0,0 +1,44 @@
+import AWS from "aws-sdk";
+import middy from "@middy/core";
+import httpJsonBodyParser from "@middy/http-json-body-parser";
+import httpEventNormalizer from "@middy/http-event-normalizer";
+import httpErrorHandler from "@middy/http-error-handler";
+import createError from "http-errors";
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+// @desc      Get auction by id
+// @route     GET /auction
+// @access    Public
+async function getAuction(event, context) {
+  let auction;
+  const { id } = event.pathParameters;
+
+  try {
+    const result = await dynamodb
+      .get({
+        TableName: process.env.AUCTIONS_TABLE_NAME,
+        Key: { id },
+      })
+      .promise();
+
+    acution = result;
+  } catch (error) {
+    console.error(error);
+    throw new createError.InternalServerError(error);
+  }
+
+  if (!auction) {
+    throw new createError.NotFound(`Auction with ID "${id}" not found!`);
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(auction),
+  };
+}
+
+export const handler = middy(getAuction)
+  .use(httpJsonBodyParser()) // Automatically parse stringified event body
+  .use(httpEventNormalizer()) // Automatically adjust the API gateway event objects
+  .use(httpErrorHandler()); // Helpes hanlding error process
