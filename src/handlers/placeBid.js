870df172204997c1d031b45247a46078e5a6ef81
salahmander <salahabdo@hotmail.co.uk>
2021-02-04 11:54:54 +0000

    Validate bid to make sure bid is higher than current bid.

--- src/handlers/placeBid.js
+++ src/handlers/placeBid.js
@@ -1,6 +1,7 @@
 import AWS from "aws-sdk";
 import createError from "http-errors";
 import commonMiddleware from "../lib/commonMiddleware";
+import { getAuctionById } from "./getAuction";
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
@@ -8,6 +9,14 @@ async function placeBid(event, context) {
   const { id } = event.pathParameters;
   const { amount } = event.body;
 
+  const auction = await getAuctionById(id);
+
+  // Bid amount validation
+  if (amount <= auction.highestBid.amount) {
+    throw new createError.Forbidden(
+      `Your bid must be higher than ${auction.highestBid.amount}!`
+    );
+  }
   const params = {
     TableName: process.env.AUCTIONS_TABLE_NAME,
     Key: { id },
