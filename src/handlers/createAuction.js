d92dc3c96cf548a17135046b2ad5977ab829748e
salahmander <salahabdo@hotmail.co.uk>
2021-02-04 13:18:25 +0000

    Rename endDate to endingAt.

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -19,7 +19,7 @@ async function createAuction(event, context) {
     title,
     status: "OPEN",
     createdAt: now.toISOString(),
-    endDate: endDate.toISOString(),
+    endingAt: endDate.toISOString(),
     highestBid: {
       amount: 0,
     },
