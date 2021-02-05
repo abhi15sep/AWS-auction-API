4573b9c7df70cd0c36586d964736ff5bcf310b0c
salahmander <salahabdo@hotmail.co.uk>
2021-02-05 13:13:25 +0000

    Send email to seller notifying that the item has been sold. Send email to winner notifying that they won the item.

--- src/lib/closeAuction.js
+++ src/lib/closeAuction.js
@@ -1,6 +1,7 @@
 import AWS from "aws-sdk";
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
+const sqs = new AWS.SQS();
 
 export async function closeAuction(auction) {
   const params = {
@@ -15,6 +16,34 @@ export async function closeAuction(auction) {
     },
   };
 
-  const result = await dynamodb.update(params).promise();
-  return result;
+  await dynamodb.update(params).promise();
+
+  const { title, seller, highestBid } = auction;
+  const { amount, bidder } = highestBid;
+
+  // Notify user that created the auction that item has been sold via email
+  const notifySeller = sqs
+    .sendMessage({
+      QueueUrl: process.env.MAIL_QUEUE_URL,
+      MessageBody: JSON.stringify({
+        subject: "Your item has been sold!",
+        recipient: seller,
+        body: `Hey ${seller}, your item "${title}" has been old for £${amount}.`,
+      }),
+    })
+    .promise();
+
+  // Notify user that won the auction via email
+  const notifyBidder = sqs
+    .sendMessage({
+      QueueUrl: process.env.MAIL_QUEUE_URL,
+      MessageBody: JSON.stringify({
+        subject: "You won an auction!",
+        recipient: bidder,
+        body: `Hey ${bidder}, you win the auction! your got yourself a "${title}" for £${amount}.`,
+      }),
+    })
+    .promise();
+
+  return Promise.all(notifySeller, notifyBidder);
 }
