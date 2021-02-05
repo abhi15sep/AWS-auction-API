7a223fdd1a33e98ad4bbbf27343a7981a2d2f3d2
salahmander <salahabdo@hotmail.co.uk>
2021-02-05 23:30:59 +0000

    Create new upload auction picture handler.

--- src/handlers/uploadAuctionPicture.js
+++ src/handlers/uploadAuctionPicture.js
@@ -1,8 +1,30 @@
+import { getAuctionById } from "./getAuction";
+import { uploadPictureToS3 } from "../lib/uploadPictureToS3";
+import middy from "@middy/core";
+import httpErrorHandler from "@middy/core/http-error-handler";
+import createError from "http-errors";
+
 export async function uploadAuctionPicture(event) {
+  const { id } = event.pathParameters;
+  const auction = await getAuctionById(id);
+  const base64 = event.body.replace(/^data:image\/\w+;base64,/, ""); // Esnure base64 string isn't corrupted
+  const buffer = Buffer.from(base64, "base64");
+
+  try {
+    const uploadToS3Result = await uploadPictureToS3(
+      auction.id + ".jpg",
+      buffer
+    );
+    console.log(uploadToS3Result);
+  } catch (error) {
+    console.error(error);
+    throw new createError.InternalServerError(error);
+  }
+
   return {
     statusCode: 200,
     body: JSON.stringify({}),
   };
 }
 
-export const handler = uploadAuctionPicture;
+export const handler = middy(uploadAuctionPicture).use(httpErrorHandler());
