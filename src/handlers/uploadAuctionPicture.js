74d53e173a517b13b424bea0b180090c73bf3d83
salahmander <salahabdo@hotmail.co.uk>
2021-02-06 12:34:54 +0000

    Update auction using picture url and make sure auction is validated before change.

--- src/handlers/uploadAuctionPicture.js
+++ src/handlers/uploadAuctionPicture.js
@@ -1,21 +1,28 @@
+import middy from "@middy/core";
+import httpErrorHandler from "@middy/http-error-handler";
+import createError from "http-errors";
 import { getAuctionById } from "./getAuction";
 import { uploadPictureToS3 } from "../lib/uploadPictureToS3";
-import middy from "@middy/core";
-import httpErrorHandler from "@middy/core/http-error-handler";
-import createError from "http-errors";
+import { setAuctionPictureUrl } from "../lib/setAuctionPictureUrl";
 
 export async function uploadAuctionPicture(event) {
   const { id } = event.pathParameters;
+  const { email } = event.requestContext.authorizer;
   const auction = await getAuctionById(id);
+
+  // Validate auction ownership
+  if (auction.seller !== email) {
+    throw new createError.Forbidden("You are not the seller of this auction!");
+  }
+
   const base64 = event.body.replace(/^data:image\/\w+;base64,/, ""); // Esnure base64 string isn't corrupted
   const buffer = Buffer.from(base64, "base64");
 
+  let updatedAuction;
+
   try {
-    const uploadToS3Result = await uploadPictureToS3(
-      auction.id + ".jpg",
-      buffer
-    );
-    console.log(uploadToS3Result);
+    const pictureUrl = await uploadPictureToS3(auction.id + ".jpg", buffer);
+    updatedAuction = await setAuctionPictureUrl(auction.id, pictureUrl);
   } catch (error) {
     console.error(error);
     throw new createError.InternalServerError(error);
@@ -23,7 +30,7 @@ export async function uploadAuctionPicture(event) {
 
   return {
     statusCode: 200,
-    body: JSON.stringify({}),
+    body: JSON.stringify(updatedAuction),
   };
 }
 
